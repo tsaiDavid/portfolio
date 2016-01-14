160547a5479b503f9f26c758eeefd8087f9c755c
David Tsai <David@whataniceguy.com>
2016-01-13 19:29:11 -0800

    Restyled the about page for preview purposes

--- shared/components/AboutView.jsx
+++ shared/components/AboutView.jsx
@@ -2,34 +2,44 @@ import React from 'react'
 
 export const AboutView = () => {
   return (
-    <div className="about-container row">
-      <div className="question-title">
-        <strong>Who are you?</strong>
-      </div>
-      <div className="question-response">
-        I'm a Full Stack Software Engineer passionate about building and shipping beautiful, thoughtful, and performant products.
+    <div className="about-container container">
 
-        I'm oriented by a holistic understanding of the full stack, utilizing tools like React, Node, and MongoDB to create products
-        guided by my deep experience in understanding user needs.
+      <div className="question row">
+        <div className="question-title twelve columns">
+          <strong>WHO</strong>
+        </div>
+        <div className="question-response twelve columns">
+          I'm a Full Stack Software Engineer passionate about building and shipping beautiful, thoughtful, and performant products.
+
+          I'm oriented by a holistic understanding of the full stack, utilizing tools like React, Node, and MongoDB to create products
+          guided by my deep experience in understanding user needs.
+        </div>
       </div>
-      <div className="question-title">
-        <strong>What excites you?</strong>
+
+      <div className="question row">
+        <div className="question-title twelve columns">
+          <strong>WHAT</strong>
+        </div>
+        <div className="question-response twelve columns">
+          I spend a lot of time considering 'developer experience' - I'm encouraged
+          by the momentum I see in the community, especially wth an increasingly positive
+          response towards the open source community. I'm particularly passionate
+          about the fact that budding technologies many of us use in production can
+          be the resulting ideas.
+        </div>
       </div>
-      <div className="question-response">
-        I spend a lot of time considering 'developer experience' - I'm encouraged
-        by the momentum I see in the community, especially wth an increasingly positive
-        response towards the open source community. I'm particularly passionate
-        about the fact that budding technologies many of us use in production can
-        be the resulting ideas.
+
+      <div className="question row">
+        <div className="question-title twelve columns">
+          <strong>WHERE</strong>
+        </div>
+        <div className="question-response twelve columns">
+          I decided to leverage some of my favorite technologies in building out this portfolio.
+          Given the nature of React and Redux, which allows easy serialization of app state, I was
+          able to incorporate server-side-rendering (also known as universal or isomoprhic).
+        </div>
       </div>
-      <div className="question-title">
-        <strong>How'd you build this portfolio?</strong>
-      </div>
-      <div className="question-response">
-        I decided to leverage some of my favorite technologies in building out this portfolio.
-        Given the nature of React and Redux, which allows easy serialization of app state, I was
-        able to incorporate server-side-rendering (also known as universal or isomoprhic).
-      </div>
+
     </div>
   )
 }
--- shared/components/Home.jsx
+++ shared/components/Home.jsx
@@ -10,15 +10,17 @@ class Home extends React.Component {
   render() {
     console.log(this.props.routing)
     return (
-      <div id="project-list">
-        <div className="video-wrapper row">
-          <img
-            className="gfyitem"
-            data-id="LankyConventionalBuck"
-            data-controls="false"
-            data-autoplay="true"
-            data-expand="true"
-          />
+      <div className="main-body">
+        <div id="project-list">
+          <div className="video-wrapper row">
+            <img
+              className="gfyitem"
+              data-id="LankyConventionalBuck"
+              data-controls="false"
+              data-autoplay="true"
+              data-expand="true"
+            />
+          </div>
         </div>
       </div>
     );
